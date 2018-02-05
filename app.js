18af671b78d48d131c16e0f06c056271b9195cb3
Aaron Wong <flamingc4@gmail.com>
2018-02-05 12:06:23 -0800

    took the starting files from the ixd page

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-var hello = require('./routes/hello');
-var project = require('./routes/project');
-
 // Example route
 // var user = require('./routes/user');
 
@@ -32,23 +29,14 @@ app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// Add routes here
 app.get('/', index.view);
-app.get('/hello/:userName', hello.view);
-app.get('/', index.view);
-//app.get("/project", project.viewProject);
-app.get('/project/:name', project.viewProject);
-
 // Example route
-
+// app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
